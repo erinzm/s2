0fe679a261326c71ab5542b52225b3ec57fdf63e
Liam Marshall <limarshall@wisc.edu>
2018-06-06 18:35:01 -0500

    util: split out draw_labeled_graph

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from s2 import s2, path_midpoint, enumerate_find_ssp
+from util import draw_labeled_graph
 
 def test_simple_lattice():
     G = nx.grid_2d_graph(10, 10)
@@ -12,21 +13,12 @@ def test_simple_lattice():
 
     fig = plt.figure()
     fig.add_subplot(121).title.set_text('Ground-truth')
-    _draw_labeled_graph(G, oracle)
+    draw_labeled_graph(G, oracle)
 
     fig.add_subplot(122).title.set_text('$S^2$')
-    _draw_labeled_graph(G_cut, lambda v: G_cut.node[v].get('label'))
+    draw_labeled_graph(G_cut, lambda v: G_cut.node[v].get('label'))
 
     plt.show()
-
-def _draw_labeled_graph(G, oracle):
-    def label_to_color(l):
-        if l is None: return '0.75'
-        return 'r' if l > 0 else 'b'
-
-    nx.draw(G,
-        pos={n: n for n in G.nodes_iter()},
-        node_color=[label_to_color(oracle(n)) for n in G.nodes_iter()])
 
 if __name__ == '__main__':
     test_simple_lattice()
---
+++ util.py
@@ -0,0 +1,10 @@
+import networkx as nx
+
+def draw_labeled_graph(G, oracle):
+    def label_to_color(l):
+        if l is None: return '0.75'
+        return 'r' if l > 0 else 'b'
+
+    nx.draw(G,
+        pos={n: n for n in G.nodes_iter()},
+        node_color=[label_to_color(oracle(n)) for n in G.nodes_iter()])
