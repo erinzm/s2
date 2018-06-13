7c08881f122b23083fb059755bf5a397f31b630b
Liam Marshall <limarshall@wisc.edu>
2018-06-13 15:51:46 -0500

    initial pass at implementing MOSS accelerated MSSP

--- example.py
+++ example.py
@@ -1,7 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from s2 import s2, path_midpoint, enumerate_find_ssp
+from moss import moss
 from util import draw_labeled_graph
+import timeit
+
 
 def test_simple_lattice():
     G = nx.grid_2d_graph(10, 10)
@@ -9,7 +12,10 @@ def test_simple_lattice():
     def oracle(vert):
         return ((vert[0] < 3) and (vert[1] < 3)) or ((vert[0] > 6) and (vert[1] > 6))
 
-    G_cut = s2(G, oracle, lambda G, U, V: path_midpoint(enumerate_find_ssp(G, U, V)))
+    # enum: 638 ms ± 22.8 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+    # moss: 18.1 ms ± 75.7 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
+
+    G_cut = s2(G, oracle, lambda G, U, V: moss(G, U, V))
 
     fig = plt.figure()
     fig.add_subplot(121).title.set_text('Ground-truth')
---
+++ moss.py
@@ -0,0 +1,31 @@
+from collections import deque
+import networkx as nx
+
+def moss(G, U, V):
+    queue_u,   queue_v = deque([]), deque([])
+    visited_u, visited_v = set(), set()
+
+    for u in U:
+        queue_u.append((u, G.neighbors_iter(u)))
+        visited_u.add(u)
+
+    for v in V:
+        queue_v.append((v, G.neighbors_iter(v)))
+        visited_v.add(v)
+
+    while queue_u and queue_v:
+        parent, children = queue_u.popleft()
+        for child in children:
+            if child not in visited_u:
+                visited_u.add(child)
+                queue_u.append((child, G.neighbors_iter(child)))
+                if child in visited_v and child not in V:
+                    return child
+
+        parent, children = queue_v.popleft()
+        for child in children:
+            if child not in visited_v:
+                visited_v.add(child)
+                queue_v.append((child, G.neighbors_iter(child)))
+                if child in visited_u and child not in U:
+                    return child
