7982cbeb66a54bab7ecba71f7d1aab4f0d9b994a
Liam Marshall <limarshall@wisc.edu>
2018-08-30 18:18:01 -0500

    schema updates

--- app/schema.sql
+++ app/schema.sql
@@ -13,12 +13,21 @@ CREATE TABLE nodes (
     exp_id bigint REFERENCES experiments(id) NOT NULL,
     image_id bigint REFERENCES images(id) NOT NULL,
     
-    basis_coordinates integer[],
-    label integer
+    basis_coordinates integer[] NOT NULL,
+    label integer NOT NULL
 );
 
 CREATE TABLE bases (
     id bigserial PRIMARY KEY,
     image_id bigint REFERENCES images(id) NOT NULL,
     uri VARCHAR NOT NULL
+);
+
+CREATE TYPE jobstatus AS ENUM ('unassigned', 'waiting', 'completed');
+CREATE TABLE jobs (
+    exp_id bigint REFERENCES experiments(id) NOT NULL,
+    node_id bigint REFERENCES nodes(id) NOT NULL,
+    
+    ballot_id int NOT NULL,
+    status jobstatus NOT NULL
 );
