122659d257170e0e2ad4cf940afd915da0987e48
Liam Marshall <limarshall@wisc.edu>
2018-09-06 11:08:38 -0500

    imgen: as_base64_png()

--- app/imgen.py
+++ app/imgen.py
@@ -1,10 +1,18 @@
-import numpy as np
 from typing import List
+from io import BytesIO
+import base64
+import numpy as np
 from PIL import Image
 
 def load_image(fp) -> np.ndarray:
     image = Image.open(fp)
     return np.asarray(image).astype('float')
+
+def as_base64_png(img: Image) -> bytes:
+    buf = BytesIO()
+    img.save(buf, format='png')
+    data = b'data:image/png;base64,' + base64.encodestring(buf.getvalue())
+    return data.decode('utf-8')
 
 def perturb_image(base_image: np.ndarray, weights: np.ndarray, bases: List[np.ndarray]):
     perturbation = np.zeros_like(base_image)
